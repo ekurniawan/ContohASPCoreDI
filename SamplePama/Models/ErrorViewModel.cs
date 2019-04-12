dc57df32ce18e9d21a0eeb543d2b6b97ff974ad8
Erick Kurniawan <erick.kurniawan@gmail.com>
2019-04-12 09:17:27 +0700

    Add project files.

---
+++ SamplePama/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SamplePama.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
